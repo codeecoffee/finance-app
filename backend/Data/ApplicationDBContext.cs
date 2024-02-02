89ea38996d96ae32e586131c236bf2d49fd57abc
codeecoffee <fellipefl7@msn.com>
2024-02-01 16:53:39 -0800

    Added Authentication

--- backend/Data/ApplicationDBContext.cs
+++ backend/Data/ApplicationDBContext.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using backend.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace backend.Data
 {
-    public class ApplicationDBContext : DbContext
+    public class ApplicationDBContext : IdentityDbContext<AppUser>
     {
-        public ApplicationDBContext(DbContextOptions dbContextOptions) 
-        :base(dbContextOptions)
+        public ApplicationDBContext(DbContextOptions dbContextOptions)
+        : base(dbContextOptions)
         {
-            
+
         }
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
